97538b3861d019566f41ce435e9c2cf3e0259e1f
Victor Brito de Sousa <victorbrito001@gmail.com>
2021-06-23 15:59:38 -0300

    Cadastro de Usuários/Cadastro de Tags

---
+++ src/routes.ts
@@ -0,0 +1,17 @@
+import { Router } from "express"
+
+import { CreateUserController } from "./controllers/CreateUserController"
+import { CreateTagController } from "./controllers/CreateTagController"
+
+import { ensureAdmin } from "./middlewares/ensureAdmin"
+
+const routes = Router()
+
+const createUserController = new CreateUserController()
+const createTagController = new CreateTagController()
+
+routes.post("/users", createUserController.handle)
+routes.post("/tags", ensureAdmin, createTagController.handle)
+
+
+export { routes }
